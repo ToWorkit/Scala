58ce297b3a25cadd442b29842f1003acbd8d7a5e
Doing <1211126374@qq.com>
2018-09-28 18:00:35 +0800

    update

--- src/main/scala/base/set.scala
+++ src/main/scala/base/set.scala
@@ -15,9 +15,21 @@ object set {
     val s2 = mutable.Set(1, 2, 1, 2, 3, 4)
     println(s2)
 
-    // 值
-    println(s2(0))
+    // 判断有木有
+    println(s1(5))
 
+    // 追加值
+    s2.add(9)
+    println(s2)
+
+    s2 += 0
+    s2.remove(2)
+    s2 -= 1
+    println(s2)
+
+    for (x <- s2) {
+      println(x)
+    }
 
   }
 }
