4bc92b479f64b6b6a77e47bac3ca23330196d415
Doing <1211126374@qq.com>
2018-09-28 14:51:34 +0800

    update

--- src/main/scala/base/exception.scala
+++ src/main/scala/base/exception.scala
@@ -2,10 +2,16 @@ package base
 
 object exception {
   def main(args: Array[String]): Unit = {
-
+    try {
+      divider(10, 0);
+    } catch {
+      case ex: RuntimeException => println("异常 " + ex.getMessage)
+    } finally {}
   }
 
   def divider (x: Int, y: Int): Unit = {
     if (y == 0) throw new RuntimeException("0不可以做除数")
+    else
+      x / y
   }
 }
